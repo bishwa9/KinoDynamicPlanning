ec45b2bc25b766f82ff587b576fa7dd571e129fd
bishwa9 <priam93@gmail.com>
2016-04-27 18:03:25 -0400

    initial commit

---
+++ AStarPlanner.py
@@ -0,0 +1,96 @@
+import heapq
+import numpy
+import time
+
+class list_object(object):
+    def __init__(self, action, parent):
+        self.ActionToGetHere = action
+        self.parent = parent
+
+class AStarPlanner(object):
+    
+    def __init__(self, planning_env, visualize):
+        self.planning_env = planning_env
+        self.visualize = visualize
+        self.nodes = dict()
+
+    def Plan(self, start_config, goal_config):
+
+        plan = []
+        # TODO: Here you will implement the AStar planner
+        #  The return path should be a numpy array
+        #  of dimension k x n where k is the number of waypoints
+        #  and n is the dimension of the robots configuration space
+        # print start_config
+        if self.visualize and hasattr(self.planning_env, 'InitializePlot'):
+            self.planning_env.InitializePlot(goal_config)
+
+        start_config_id = int(self.planning_env.discrete_env.ConfigurationToNodeId(start_config))
+        goal_config_id = int(self.planning_env.discrete_env.ConfigurationToNodeId(goal_config))
+        begin_config_id = start_config_id
+
+        q = []
+        index = 0
+
+        no_vertices = reduce(lambda x, y: x * y, self.planning_env.discrete_env.num_cells, 1)
+
+        parent = dict()
+        dist_from_start = dict()
+
+        heuristic_ = self.planning_env.ComputeHeuristicCost(goal_config_id,start_config_id)
+        heapq.heappush(q, (0, index, start_config_id))
+        index += 1
+        parent[start_config_id] = -1
+        dist_from_start[start_config_id] = 0
+        num_nodes = 0
+
+        while len(q) > 0:
+            current = heapq.heappop(q)[-1]
+            dist_till_current = dist_from_start[current]
+            num_nodes += 1
+            successors, successors_actions = self.planning_env.GetSuccessors(current)
+            for action_idx in range(len(successors_actions)):
+                action = successors_actions[action_idx]
+                #path that was rolled out by the action
+                neighbor_path = successors[action_idx]
+
+                #collision check
+                # Get the current transform
+                T = self.planning_env.robot.GetTransform()
+                inCollision = False
+                for fp_idx in range(len(neighbor_path) - 1):
+                    id1 = neighbor_path[fp_idx]
+                    id2 = neighbor_path[fp_idx+1]
+                    start_config = self.planning_env.discrete_env.NodeIdToConfiguration(id1)
+                    end_config = self.planning_env.discrete_env.NodeIdToConfiguration(id2)
+                    if self.planning_env.checkCollision(start_config, end_config):
+                        inCollision = True
+                        break
+                 
+                neighbor_id = neighbor_path[-1]
+                
+                if neighbor_id not in parent and not inCollision:
+                    parent[neighbor_id] = list_object(action, current)
+                    # Plotting function
+                    # if self.visualize:
+                    #     self.planning_env.PlotEdge(start_config, end_config)
+                    heuristic_ = self.planning_env.ComputeHeuristicCost(goal_config_id,neighbor_id)
+                    cost_till = self.planning_env.ComputeDistance(current, neighbor_id) + dist_till_current
+                    dist_from_start[neighbor_id] = cost_till
+                    heapq.heappush(q, (heuristic_+cost_till/10.0, index, neighbor_id))
+                    index += 1
+                    if neighbor_id == goal_config_id:
+                        root = neighbor_id
+                        while root != -1:
+                            plan.append(action)
+                            parent_node = parent[root]
+                            root = parent_node
+                            action = parent_node.ActionToGetHere
+                        # if self.visualize and hasattr(self.planning_env, 'InitializePlot'):
+                        #     self.planning_env.PlotFinal(plan[::-1])
+                        return plan[::-1]
+        print "Done nothing"
+
+    def hueristic(self, start_config, end_config):
+        dist = sum([abs(x1 - x2) for x1, x2 in zip(start_config, end_config)])
+        return dist
---
+++ DiscreteEnvironment.py
@@ -0,0 +1,88 @@
+import numpy
+import time
+import math
+
+class DiscreteEnvironment(object):
+
+    def __init__(self, resolution, lower_limits, upper_limits):
+
+        # Store the resolution
+        self.resolution = resolution
+
+        # Store the bounds
+        self.lower_limits = lower_limits
+        self.upper_limits = upper_limits
+
+        # Calculate the dimension
+        self.dimension = len(self.lower_limits)
+
+        # Figure out the number of grid cells that are in each dimension
+        self.num_cells = self.dimension*[0]
+        for idx in range(self.dimension):
+            self.num_cells[idx] = numpy.ceil((upper_limits[idx] - lower_limits[idx])/resolution[idx])
+
+    def ConfigurationToNodeId(self, config):
+
+        # TODO:
+        # This function maps a node configuration in full configuration
+        # space to a node in discrete space
+        #
+        node_id = self.GridCoordToNodeId(self.ConfigurationToGridCoord(config))
+        return node_id
+
+    def NodeIdToConfiguration(self, nid):
+
+        # TODO: Done
+        # This function maps a node in discrete space to a configuraiton
+        # in the full configuration space
+        #
+        config = self.GridCoordToConfiguration(self.NodeIdToGridCoord(nid))
+        return config
+
+    def ConfigurationToGridCoord(self, config):
+
+        # TODO:
+        # This function maps a configuration in the full configuration space
+        # to a grid coordinate in discrete space
+        #
+        # coord = [0] * self.dimension
+
+        # Check around
+        coord = [numpy.floor((i - self.lower_limits[index])/self.resolution[index]) for index, i in enumerate(config)]
+        return coord
+
+    def GridCoordToConfiguration(self, coord):
+
+        # TODO:
+        # This function maps a grid coordinate in discrete space
+        # to a configuration in the full configuration space
+        #
+        # config = [0] * self.dimension
+        config = [(i+1/2.)*self.resolution[index] + self.lower_limits[index] for index, i in enumerate(coord)]
+        return config
+
+    def GridCoordToNodeId(self, coord):
+
+        # TODO:
+        # This function maps a grid coordinate to the associated
+        # node id
+
+        node_id = coord[0] + sum([reduce(lambda x, y: x * y, self.num_cells[:i], 1)*coord[i] for i in range(1, len(coord))])
+        return node_id
+
+    def NodeIdToGridCoord(self, node_id):
+
+        # TODO:
+        # This function maps a node id to the associated
+        # grid coordinate
+
+        coord = []
+        divider = 1
+        remainder = node_id
+        for i in range(1, len(self.num_cells))[::-1]:
+            divider = reduce(lambda x, y: x * y, self.num_cells[:i], 1)
+            coord.append(math.floor(remainder/divider))
+            remainder = remainder % divider
+        coord.append(int(remainder))
+        coord = coord[::-1]
+        return coord
